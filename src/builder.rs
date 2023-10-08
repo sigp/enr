028ad79e608f26077fb9575c4524ac4e0dd6c8b8
Diva M <divma@protonmail.com>
2023-10-08 15:54:48 -0500

    rustdoc

--- src/builder.rs
+++ src/builder.rs
@@ -99,7 +99,7 @@ impl<K: EnrKey> EnrBuilder<K> {
     /// Constructs an ENR from the `EnrBuilder`.
     ///
     /// # Errors
-    /// Fails if the identity scheme is not supported, or the record size exceeds [`MAX_ENR_SIZE`].
+    /// Fails if the identity scheme is not supported, or the record size exceeds [`super::MAX_ENR_SIZE`].
     pub fn build(&self, signing_key: &K) -> Result<Enr<K>, EnrError> {
         let mut enr = self.enr.clone();
         let updates = self.updates.clone();
