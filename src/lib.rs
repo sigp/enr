e48bff60e7f751ed842b992e79e2a37cb9e04ea7
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-03-28 00:37:30 +0100

    perf: remove unnecessary clones

--- src/lib.rs
+++ src/lib.rs
@@ -986,48 +986,47 @@ impl<K: EnrKey> Decodable for Enr<K> {
         if payload.is_empty() {
             return Err(DecoderError::Custom("Payload is empty"));
         }
-
-        let signature = Bytes::decode(payload)?;
+        let signature = Header::decode_bytes(payload, false)?;
 
         if payload.is_empty() {
             return Err(DecoderError::Custom("Seq is missing"));
         }
-
         let seq = u64::decode(payload)?;
 
         let mut content = BTreeMap::new();
-        let mut prev: Option<Vec<u8>> = None;
+        let mut prev = None;
         while !payload.is_empty() {
-            let key = Bytes::decode(payload)?;
-            let raw_key = key.to_vec();
-            if prev.is_some() && prev >= Some(raw_key.clone()) {
-                return Err(DecoderError::Custom("Unsorted keys"));
+            let key = Header::decode_bytes(payload, false)?;
+            if let Some(prev) = prev {
+                if prev >= key {
+                    return Err(DecoderError::Custom("Unsorted keys"));
+                }
             }
-            prev = Some(raw_key.clone());
+            prev = Some(key);
 
-            match raw_key.as_slice() {
+            let value = match key {
                 b"id" => {
-                    let id = Bytes::decode(payload)?;
-                    if id.to_vec() != b"v4" {
+                    let id = Header::decode_bytes(payload, false)?;
+                    if id != b"v4" {
                         return Err(DecoderError::Custom("Unsupported identity scheme"));
                     }
-                    content.insert(raw_key, Bytes::copy_from_slice(&alloy_rlp::encode(id)));
+                    alloy_rlp::encode(id)
                 }
                 b"tcp" | b"tcp6" | b"udp" | b"udp6" => {
                     let port = u16::decode(payload)?;
-                    content.insert(raw_key, Bytes::copy_from_slice(&alloy_rlp::encode(port)));
+                    alloy_rlp::encode(port)
                 }
                 b"ip" => {
                     let ip = Ipv4Addr::decode(payload)?;
-                    content.insert(raw_key, Bytes::copy_from_slice(&alloy_rlp::encode(ip)));
+                    alloy_rlp::encode(ip)
                 }
                 b"ip6" => {
                     let ip6 = Ipv6Addr::decode(payload)?;
-                    content.insert(raw_key, Bytes::copy_from_slice(&alloy_rlp::encode(ip6)));
+                    alloy_rlp::encode(ip6)
                 }
                 b"secp256k1" | b"ed25519" => {
-                    let keys = Bytes::decode(payload)?;
-                    content.insert(raw_key, Bytes::copy_from_slice(&alloy_rlp::encode(keys)));
+                    let keys = Header::decode_bytes(payload, false)?;
+                    alloy_rlp::encode(keys)
                 }
                 b"eth" => {
                     let eth_header = Header::decode(payload)?;
@@ -1037,18 +1036,19 @@ impl<K: EnrKey> Decodable for Enr<K> {
                         list: true,
                         payload_length: value.len(),
                     };
-                    let mut out = BytesMut::new();
+                    let mut out = Vec::<u8>::new();
                     val_header.encode(&mut out);
                     out.extend_from_slice(value);
-                    content.insert(raw_key, Bytes::copy_from_slice(&out));
+                    out
                 }
                 _ => {
                     let other_header = Header::decode(payload)?;
-                    let value = &mut &payload[..other_header.payload_length];
+                    let value = &payload[..other_header.payload_length];
                     payload.advance(other_header.payload_length);
-                    content.insert(raw_key, Bytes::copy_from_slice(&alloy_rlp::encode(value)));
+                    alloy_rlp::encode(value)
                 }
             };
+            content.insert(key.to_vec(), Bytes::from(value));
         }
 
         // verify we know the signature type
