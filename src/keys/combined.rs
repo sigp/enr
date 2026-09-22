9d40a9b55cc0c8cf21825f7f2842c93e4c959147
Daniel Knopik <daniel@dknopik.de>
2026-09-22 17:05:58 +0200

    remove unneeded `ref`

--- src/keys/combined.rs
+++ src/keys/combined.rs
@@ -43,8 +43,8 @@ impl EnrKey for CombinedKey {
     /// they are supported.
     fn sign_v4(&self, msg: &[u8]) -> Result<Vec<u8>, SigningError> {
         match self {
-            Self::Secp256k1(ref key) => key.sign_v4(msg),
-            Self::Ed25519(ref key) => key.sign_v4(msg),
+            Self::Secp256k1(key) => key.sign_v4(msg),
+            Self::Ed25519(key) => key.sign_v4(msg),
         }
     }
 
